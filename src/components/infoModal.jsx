195154994c0e1929ab5793a669bc9e8a7725dd15
VictorNathan12 <victorpereiracamar@gmail.com>
2025-12-09 09:34:48 -0300

    Add files via upload

---
+++ src/components/infoModal.jsx
@@ -0,0 +1,48 @@
+import s from "./infoModal.module.css"
+
+export const InfoModal = ({ data, close, onNext, onPrev, hasNext, hasPrev }) => {
+    return (
+        <div className={s.wrapInfo}>
+            
+            {/* Botão de fechar */}
+            <button className={s.closeBtn} onClick={close}>❌</button>
+
+            {/* Botões de navegação */}
+            <div className={s.navButtons}>
+                <button 
+                    onClick={onPrev} 
+                    disabled={!hasPrev}
+                    className={s.navBtn}
+                >
+                    ◀ Prev
+                </button>
+
+                <button 
+                    onClick={onNext} 
+                    disabled={!hasNext}
+                    className={s.navBtn}
+                >
+                    Next ▶
+                </button>
+            </div>
+
+            <div className={s.wrapIndoData}>
+                <img src={data.image} alt={data.name} />
+                <h4><strong>Name: </strong>{data.name}</h4>
+                <p><strong>Status: </strong>{data.status}</p>
+                <p><strong>Origin: </strong>{data.origin.name}</p>
+                <p><strong>Species: </strong>{data.species}</p>
+                <p><strong>Gender: </strong>{data.gender}</p>
+                <p><strong>Location: </strong>{data.location.name}</p>
+                <p>
+                    <strong>Created: </strong>
+                    {new Date(data.created).toLocaleString("pt-BR", {
+                        day: "2-digit",
+                        month: "2-digit",
+                        year: "numeric"
+                    })}
+                </p>
+            </div>
+        </div>
+    )
+}
